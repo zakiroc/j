c4d8dd3a14dd5d78edf98eb752530e81ecdcf0bf
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-06-18 22:23:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+// 1#
+
+// let a = 10
+// let b = 20
+
+//     console.log(`${a} + ${b} = ${a+b}`);
+
+// 2#
+// let a = "12"
+// let b = +a
+// console.log();(`${b}, ${typeof(b)}`)
+
+
+
+// 3#
+
+
+// let random_number = Math.floor(Math.random() * 100) + 1;
+
+
+// if (random_number % 2 === 0) {
+//   console.log(`Случайное число ${random_number} является четным.`);
+// } else {
+//   console.log(`Случайное число ${random_number} является нечетным.`);
+// }
+
+
+
+
+
+
+// 4#
+
+
+// let pr  = prompt('bitta soz kiriting')
+// let s = 'Mars'
+// if(pr == s){
+//     alert ('Mars')
+// }else{
+//     alert('mars it school')
+// }
+
+
+// 5#
+
+
+// let wordsArray = ['strawberry', 'kiwi', 'melon', 'pear', 'blueberry'];
+
+// console.log('Исходный массив:', wordsArray);
+
+
+
+
+// let longWordsArray = [];
+
+
+// let shortWordsArray = [];
+
+// for (let i = 0; i < wordsArray.length; i++) {
+   
+//     if (wordsArray[i].length >= 5) {
+      
+//         longWordsArray.push(wordsArray[i]);
+//     } else {
+       
+//         shortWordsArray.push(wordsArray[i]);
+//     }
+// }
+
+
+// console.log('Слова с длиной 5 и больше букв:', longWordsArray);
+// console.log('Слова с длиной 4 и меньше букв:', shortWordsArray);
+
+
+// 6#
+
+
+// let evenNumbers = [];
+// let oddNumbers = [];
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+       
+//         evenNumbers.push(i);
+//     } else {
+     
+//         oddNumbers.push(i);
+//     }
+// }
+
+
+// console.log('Четные числа:', evenNumbers);
+// console.log('Нечетные числа:', oddNumbers);
+
+   
